8b8f4cedfe063d58678cdc61fffd3fc5a9fe12ea
basfed <basfedlan@gmail.com>
2016-08-25 23:59:34 +0800

    verify corporation

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -2,7 +2,7 @@
 var Corporation = require('../../models/corporation.js');
 
 module.exports = {
-    // 管理员首页
+    // 审核单位（管理员首页）
     index: function* () {
         try {
             // 查询未审核单位
@@ -13,7 +13,38 @@ module.exports = {
                 'result': result
             });
         } catch(e) {
-            this.body = '未审核单位页面加载失败';
+            this.body = '审核单位页面加载失败';
+            console.log(e);
+        }
+    },
+    // 审核单位详情
+    verify: function* () {
+        try {
+            var id = this.params.id;// 单位 ID
+
+            // 查询符合条件的第一条单位记录
+            var result = yield Corporation.findOne({'_id': id});
+
+            yield this.render('admin/verify', {
+                'title': '审核单位详情',
+                'result': result
+            });
+        } catch(e) {
+            this.body = '审核单位详情页面加载失败';
+            console.log(e);
+        }
+    },
+    // 审核单位处理
+    doVerify: function* () {
+        try {
+            var result = this.request.body;
+            var id = result._id;// 单位 ID
+            var state = result.state;// 审核结果
+
+            // 重定向到审核单位页面
+            this.redirect('/admin/');
+        } catch(e) {
+            this.body = '审核单位处理失败';
             console.log(e);
         }
     }
--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,8 @@ var body = require('koa-body')({ multipart: true });
 module.exports = function (router) {
     // 审核单位（管理员首页）
     router.get('/admin/', userLogin.adminLogined, adminIndex.index);
+    router.get('/admin/verifyCorporation/:id/', userLogin.adminLogined, adminIndex.verify);
+    router.post('/admin/doVerifyCorporation/', userLogin.adminLogined, body, adminIndex.doVerify);
 
     // 用户管理
 
